a26de7baf5828a02cc6233d336eb7479f15a4ad3
Faizal Luthfi <zalluth@gmail.com>
2019-12-31 14:02:16 +0700

    fix store deploy notification

--- index.js
+++ index.js
@@ -28,35 +28,41 @@ const eventToBuild = (data) => {
 
 // createSlackMessage creates a message from a build object.
 const createSlackMessage = (build) => {
-  const repo = build.source.repoSource;
-  const source = build.sourceProvenance.resolvedRepoSource;
-  const triggerId = build.buildTriggerId;
-
-  const triggerUrl = `https://console.cloud.google.com/cloud-build/triggers/edit/${triggerId}?project=${source.projectId}`;
-
-  const repoUrl = `https://source.cloud.google.com/${source.projectId}/${source.repoName}`;
-  const commitUrl = `${repoUrl}/+/${source.commitSha}`;
-  const branchUrl = `${repoUrl}/+/${repo.branchName}`;
-
-  let buildName = build.substitutions._BUILD_NAME ? build.substitutions._BUILD_NAME : build.buildTriggerId;
-
-  const triggerNameAndUrl = `<${triggerUrl}|${buildName}>`;
+  const statusAndUrl = `<${build.logUrl}|${build.status}>`;
   const buildIdAndUrl = `<${build.logUrl}|${build.id}>`
 
-  const repoNameAndUrl = `<${repoUrl}|${repo.repoName}>`;
-  const repoBranchAndUrl = `<${branchUrl}|${repo.branchName}>`;
-  const statusAndUrl = `<${build.logUrl}|${build.status}>`;
+  if (build.source) {
+    const repo = build.source.repoSource;
+    const source = build.sourceProvenance.resolvedRepoSource;
+    const triggerId = build.buildTriggerId;
 
-  const message = {
-    text: `${triggerNameAndUrl} ${statusAndUrl} build ${buildIdAndUrl}`,
-    mrkdwn: true,
-    attachments: [
-      {
-        title: build.status,
-        title_link: build.logUrl,
-        text: `Commit to ${repoNameAndUrl} on branch ${repoBranchAndUrl}`
-      }
-    ]
+    const triggerUrl = `https://console.cloud.google.com/cloud-build/triggers/edit/${triggerId}?project=${source.projectId}`;
+
+    const repoUrl = `https://source.cloud.google.com/${source.projectId}/${source.repoName}`;
+    const commitUrl = `${repoUrl}/+/${source.commitSha}`;
+    const branchUrl = `${repoUrl}/+/${repo.branchName}`;
+
+    let buildName = build.substitutions._BUILD_NAME ? build.substitutions._BUILD_NAME : build.buildTriggerId;
+
+    const triggerNameAndUrl = `<${triggerUrl}|${buildName}>`;
+
+    const repoNameAndUrl = `<${repoUrl}|${repo.repoName}>`;
+    const repoBranchAndUrl = `<${branchUrl}|${repo.branchName}>`;
+
+    return {
+      text: `${triggerNameAndUrl} ${statusAndUrl} build ${buildIdAndUrl}`,
+      mrkdwn: true,
+      attachments: [
+        {
+          title: build.status,
+          title_link: build.logUrl,
+          text: `Commit to ${repoNameAndUrl} on branch ${repoBranchAndUrl}`
+        }
+      ]
+    };
+  }
+
+  return {
+    text: `Deploy store ${statusAndUrl} build ${buildIdAndUrl}`
   };
-  return message;
 }
